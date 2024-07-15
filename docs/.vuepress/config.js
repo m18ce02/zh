909d8f64c8998cf7a949a73acea3a32b53b88a74
havid.zheng <havid.zheng@mac.sz>
2024-07-15 09:50:03 +0800

    change to zh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ module.exports = {
   // theme: require.resolve('../../vdoing'), // 使用本地主题 (先将vdoing主题文件下载到本地：https://github.com/xugaoyi/vuepress-theme-vdoing)
   title: "aiM18 Developer Center",
   description: 'Make Something People Want',
-  // base: '/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  base: "/zh/", // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
   head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
     ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
     ['meta', { name: 'keywords', content: 'erp,bpm,hcm,ebi' }],
@@ -17,7 +17,7 @@ module.exports = {
   // 主题配置
   themeConfig: {
     nav: [
-      { text: 'DOC Home', link: '/', items: [{ text: '文档主页', link: '/pages/7486ac/' }] },
+      { text: '文档主页', link: '/', items: [{ text: '文档主页', link: '/pages/7486ac/' }] },
       {
         text: 'App Development', link: '/pages/7488ac/', items: [
 		  {
